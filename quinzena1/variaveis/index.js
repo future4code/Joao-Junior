c7a2d9e0e6ccd890ad7c8a86c6c89c11eb8e8cd9
Your Name <you@example.com>
2021-07-15 20:55:09 -0300

    versao 1

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,13 @@
+/*a) Declare uma variável para armazenar um nome, sem atribuir um valor.
+
+b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+
+c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando `typeof`.
+
+d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.*/
+// acredito que por conta do valor não ter sido atribuido.
+const nome = prompt("olá, qual o seu nome?")
+const idade = prompt("Qual a sua idade?")
+console.log("Olá", nome, "voce tem",idade,"anos de idade.")
+
+
