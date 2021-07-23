7406cc5ee5463722f292f778ccfe4bb4984253de
Your Name <you@example.com>
2021-07-22 22:05:58 -0300

    level completed

--- quinzena1/strings-arrays/index.js
+++ quinzena1/strings-arrays/index.js
@@ -1,5 +1,5 @@
 /* Exercícios de interpretação de código. 
-1.
+  1.
 let array
 console.log('a. ', array) Resp: a. undefined
 
@@ -18,10 +18,45 @@ console.log('e. ', array) Resp: e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const valor = array[i+6]
 console.log('f. ', valor) Resp: f. 9
 
-2.
+  2.
 const frase = prompt("Digite uma frase")
 console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
 
 Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em marrocos"
  Resp: SUBI NUM ONIBUS EM MIRROCOS 27
 */
+/* Exercicio de escrita de código
+   1.
+
+let nome = prompt("Qual o seu nome?")
+let email = prompt("Qual o seu email?")
+console.log(`O e-mail ${email} foi cadastrado com sucesso. 
+Seja bem-vindo, ${nome}!`)
+   2.*/
+
+
+/*
+   a) e b) //console.log(favFood)
+console.log(`Essas são as minhas comidas preferidas:
+${favFood[0]}
+${favFood[1]}
+${favFood[2]}
+${favFood[3]}
+${favFood[4]}`) */
+// c)
+/* let food = ["moqueca", "lasanha", "caldo de camarão", "churrasco", "Pão-de-alho"]
+let usuario = prompt("Qual a sua comida favorita?")
+food.splice(1,1,usuario)
+console.log(`Então a nova lista é ${food}`) */
+
+// 3.
+let listaDeTarefas = []
+let tarefa1 = prompt("primeira tarefa?")
+let tarefa2 = prompt("Segunda tarefa")
+let tarefa3 = prompt("terceira tarefa")
+
+listaDeTarefas.push(tarefa1,tarefa2,tarefa3)
+console.log(listaDeTarefas)
+let usuario = prompt("Qual tarefa voce ja realizou?")
+listaDeTarefas.splice(usuario,1)
+console.log(`A nova lista é ${listaDeTarefas}`)
