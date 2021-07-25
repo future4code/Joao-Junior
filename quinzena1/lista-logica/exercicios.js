84635062aa15dac0b9d19eae57ea1f88019fc28a
João Hélio <bsbvjoao@gmail.com>
2021-07-25 18:53:55 -0300

    Q 11 approved

--- quinzena1/lista-logica/exercicios.js
+++ quinzena1/lista-logica/exercicios.js
@@ -30,32 +30,49 @@ function calculaAreaRetangulo() {
 
 // Exercício 2
 function imprimeIdade() {
-  // escreva seu código aqui
+  const anoAtual = Number(prompt('valor'))
+  const anoNascimento = Number(prompt('valor'))
+  const result = anoAtual - anoNascimento
+  console.log(result)
 }
 
 // Exercício 3
 function calculaIMC() {
-  // escreva seu código aqui
+  const peso = Number(prompt('valor'))
+  const altura = Number(prompt('valor'))
+  const resultado = peso/(altura*altura)
+  console.log(resultado)
 }
 
 // Exercício 4
 function imprimeInformacoesUsuario() {
-  // escreva seu código aqui
+  const nome = prompt('texto')
+  const idade = Number(prompt('texto'))
+  const email = prompt('text')
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
 
 // Exercício 5
 function imprimeTresCoresFavoritas() {
-  // escreva seu código aqui
+  const cor1 = prompt('texto')
+  const cor2 = prompt('texto')
+  const cor3 = prompt('texto')
+  const todas = [cor1,cor2,cor3]
+  console.log(todas)
 }
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
-  // escreva seu código aqui
+  const texto = prompt('texto')
+  console.log(texto.toLocaleUpperCase())
 }
 
 // Exercício 7
 function calculaIngressosEspetaculo() {
-  // escreva seu código aqui
+  const custo = Number(prompt('txt'))
+  const ingresso = Number(prompt('txt'))
+  const resultado = custo/ingresso 
+  console.log(resultado)
 }
 
 // Exercício 8
@@ -77,12 +94,22 @@ function checaIgualdadeDesconsiderandoCase() {
 
 // Exercício 10
 function checaRenovacaoRG() {
-  // escreva seu código aqui
+  const anoAtual = Number(prompt('texto'))
+  const anoNascimento = Number(prompt('texto'))
+  const anoRg = Number(prompt('texto'))
+
+  const renova20Anos = anoAtual - anoNascimento <= 20 && anoAtual- anoRg <= 5
+  const renovacao50Anos = anoAtual - anoNascimento > 20 <= 50 && anoAtual - anoRg <=10
+  const renovacao50AnosMais = anoAtual - anoNascimento > 50 && anoAtual - anoRg <= 15
+  const renovacaoRg = renova20Anos || renovacao50Anos || renovacao50AnosMais
+  console.log(renovacaoRg)
 }
 
 // Exercício 11
 function checaAnoBissexto() {
-  // escreva seu código aqui
+  const ano = Number(prompt('escreva'))
+  const bissexto = (ano % 4 == 0) == (ano % 100 == 0) == (ano % 400 == 0)
+  console.log(bissexto)
 }
 
 // Exercício 12
