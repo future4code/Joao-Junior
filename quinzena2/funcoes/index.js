c4caf41ed37478bfd2da32e5dccb2ecef396775a
João Hélio <bsbvjoao@gmail.com>
2021-07-27 19:47:54 -0300

    nivel 2

--- quinzena2/funcoes/index.js
+++ quinzena2/funcoes/index.js
@@ -1,59 +1,73 @@
-/* function minhaFuncao(variavel) {
+/* 
+1. Exercícios de interpretação de código
+
+function minhaFuncao(variavel) {
 	return variavel * 5
 }
-
 console.log(minhaFuncao(2))
-console.log(minhaFuncao(10)) */
+console.log(minhaFuncao(10)) 
 
-/* let textoDoUsuario = prompt("Insira um texto");
+a) O que vai ser impresso no console?
+Resp: 10 50 
+b) Não mosta o que está dentro da função
+*/
 
-const outraFuncao = function(texto) {
-	return texto.toLowerCase().includes("cenoura")
-}
+/* 2.
+a) Retorna um booleano pelo qual afirma verdadeiro caso a palavra cenoura seja inputada pelo usuario.
+b)
+i. True
+ii.True
+iii.Ture
+ */
+// Exercício de Escrita de código
 
-const resposta = outraFuncao(textoDoUsuario)
-console.log(resposta)
- a função retorna se o usuario digitou cenoura ou nao */
-
- /* function perfil(nome, idade, local, estudante){
+// 1.
+// a)
+ function perfil(nome, idade, local, estudante){
      console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${local} e sou ${estudante}.`)
  }
+ console.log(perfil("Joao", 30, "Salvador", "estudante"))
 
- console.log(perfil("Joao", 30, "Salvador", "estudante")) */
+ // b)
+ function perfil(nome, idade, local, estudante){
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${local} e sou ${estudante}.`)
+}
+console.log(perfil("Joao", 30, "Salvador", "estudante"))
 
- //a) Escreva uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das duas entradas e retorne o resultado. Invoque a função e imprima no console o resultado.
+// 2.
+ // a) Escreva uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das duas entradas e retorne o resultado. Invoque a função e imprima no console o resultado.
 
-/*  function soma(n1,n2){
-     console.log(n1+n2)
+ function soma(n1,n2){
+     return n1+n2
  }
 
- console.log(soma(5,10)) */
+ console.log(soma(5,10))
 
- //b) Faça uma função que recebe 2 números e retorne um booleano que informa se o primeiro número é maior ou igual ao segundo.
+ // b) Faça uma função que recebe 2 números e retorne um booleano que informa se o primeiro número é maior ou igual ao segundo.
 
-/*  function operacao(n1,n2){     
-     console.log(n1>=n2?true:false)
+ function operacao(n1,n2){     
+     return n1>=n2
  }
 
- console.log(operacao(7,3)) */
+ console.log(operacao(7,3))
 
- //c) Escreva uma função que receba um número e devolva um booleano indicando se ele é par ou não.
+ // c) Escreva uma função que receba um número e devolva um booleano indicando se ele é par ou não.
 
-/*  function resultado(n1,n2){
-     console.log(n1%n2==0? "par":"impar")
+ function resultado(n1,n2){
+     return n1%n2==0
  }
 
- console.log(resultado(3,2)) */
+ console.log(resultado(3,2))
 
- //d) Faça uma função que recebe uma mensagem (string) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela em letras maiúsculas.
-/* 
+ // d) Faça uma função que recebe uma mensagem (string) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela em letras maiúsculas.
+
  function msg(mensagem){
      console.log(mensagem.toLowerCase(), mensagem.length)
  }
 
  console.log(msg("casualidade"))
- */
- /* 3. Crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão). Em seguida, peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário sendo o argumento. Por fim, mostre no console o resultado das operações:
+
+ /*  3. Crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão). Em seguida, peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário sendo o argumento. Por fim, mostre no console o resultado das operações:
 
     ```
     Números inseridos: 30 e 3
@@ -82,3 +96,7 @@ console.log(resposta)
         return dd1/dd2
     }
     console.log(`Divisão:` , divisao(10,5))
+
+    
+   
+    
