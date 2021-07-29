6c104dbc7521293c825f3dccfd9c5f50e29a6ce8
João Hélio <bsbvjoao@gmail.com>
2021-07-29 20:48:58 -0300

    exercicio de interpretação concluído

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,23 @@
+// 1. Exercícios de interpretação de código
+/*
+a)
+console.log(filme.elenco[0]) imprime Matheus Nachtergaele.
+console.log(filme.elenco[filme.elenco.length - 1)]) imprime "Matheus Nachtergaele", "Selton Mello", "Denise Fraga".
+console.log(filme.transmissoesHoje[2]) imprime Canal: "Globo", horario: "14h"
+
+  2.
+a) console.log(cachorro) imprime nome: "Juca", idade: 3, raca: "SRD"
+   console.log(gato) imprime nome: "Juba", idade:3, raca: "SRD"
+   console.log(tartaruga) imprime nome: "Jubo", idade:3, raca: "SRD"
+b) Realiza cópia de um objeto para o outro podendo também modificar e/ou adicionar algumas chaves
+
+  3.
+a)false 
+  undefined.
+b) Acreito que tenha aparecido o false porque na constante pessoa, a chave backender está definida com o booleano false. Ja no segundo console deu undefined porque não houve definição para a proprieda "altura".
+
+
+
+
+
+*/
