2112e5497cb7b3b5676b844390257749b5f8edd5
João Hélio <bsbvjoao@gmail.com>
2021-08-05 21:32:37 -0300

    Exercicio de interpretação concluído

---
+++ quinzena2/loops/loop.js
@@ -0,0 +1,27 @@
+//                  Exercícios de interpretação de código
+// 1. o código está incrementando valor em loop até o número 4, após, irá imprimir valor + i sendo i == 4.  
+/* 2.
+a) [19,21,23,25,30]
+b)const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero[i] of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+3.
+*
+**
+***
+****
+                    Exercícios de escrita de código
+*/
+// 1.
+
+let bichos = Number(prompt('Quantos animais voce tem?'))
+if ( bichos == 0 ){
+    console.log('Que pena. você pode adotar um pet!')
+} else if (bichos > 0){
+    let animais = prompt("digite o nome dos seus animais")
+    console.log(animais)
+}
