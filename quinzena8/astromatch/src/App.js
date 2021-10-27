63c08e3c6e2bfbee868e225602d1b3e0a36facf9
João Hélio <bsbvjoao@gmail.com>
2021-10-26 22:21:24 -0300

    Tela inicial básica concluída

--- quinzena8/astromatch/src/App.js
+++ quinzena8/astromatch/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
+import imagem from './imagens/imagemteste.jpg'
+
 
 function App() {
+
   return (
     <div className="App">
-       Testando 
+       <div className="container">
+
+         <div className='header'>
+            <h1 className="astromatch-head">astromatch</h1>
+            <button className='button-contatos'> Contatos </button>
+         </div>
+         
+         <img  className='imagem-avatar' src={imagem} alt="imagem-teste" />
+
+          <div>
+            <button>cancela</button>
+            <button>Aceita</button>
+          </div>
+         
+
+
+       </div>
     </div>
   );
 }
