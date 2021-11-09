8055d6cad837f3f84911d9a2c8b3db11fa283089
João Hélio <bsbvjoao@gmail.com>
2021-11-09 20:51:08 -0300

    inicializando implementação de local storage

--- quinzena9/labex/src/pages/Login.jsx
+++ quinzena9/labex/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Container = styled.div`
     text-align: center;
@@ -15,14 +16,28 @@ width: 100%;
 
 export default function Login(){
 
+    const [ email, setEmail] = useState("")
+    const [ password, setPassword] = useState("")
     const navigate = useNavigate()
+
+    const login = () => {
+        const body = {
+            email,
+            password
+        }
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/:joao-helio-banu/login").then(response => {
+            localStorage.setItem("token", response.data.token)
+        }).catch(err => {
+            return console.log(err)
+        })
+    }
 
     return(
         <Container>
         <h1>Login</h1>
         <InputContainer>
             <input type="text" />
-            <input type="password" name="password" placeholder='' id="" />
+            <input type="password" name="password" placeholder='Senha' id="nav" />
         </InputContainer>
 
         <div>
