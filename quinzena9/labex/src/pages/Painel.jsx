b8975409060df33d2c8b6451ce0f88557312617d
João Hélio <bsbvjoao@gmail.com>
2021-11-11 21:15:45 -0300

    ajustes do Painel Admn

--- quinzena9/labex/src/pages/Painel.jsx
+++ quinzena9/labex/src/pages/Painel.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    text-align: center;
+    width: 100vw;
+    `
 
 export default function Painel(){
 
     const navigate = useNavigate()
 
     return (
-        <div>
+        <Container>
+            <h1>Painel Administrativo</h1>
             <button onClick={() => navigate("/login")}>voltar</button>
             <button>Criar Viagem</button>
             <button>Logout</button>
-        </div>
+        </Container>
     )
 }
 
